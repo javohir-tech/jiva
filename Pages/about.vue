54f206071db243228b6072774dbeeac0ea360526
Suvonov Javohir <suvonovjavohir625@gmail.com>
2025-05-14 20:16:27 +0500

    seo optimization

--- Pages/about.vue
+++ Pages/about.vue
@@ -1,3 +1,9 @@
+<script lang="ts" setup>
+useHead({
+    title: "About Page"
+})
+</script>
+
 <template>
     <div>
         about page
--- Pages/index.vue
+++ Pages/index.vue
@@ -1,5 +1,7 @@
 <script setup lang="ts">
-
+useHead({
+    title:"Home Page"
+})
 </script>
 <template>
     <h1>hello nuxt</h1>
