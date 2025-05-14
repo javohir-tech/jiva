db3e3c73738d8899f45966c1e8d97f3feb4592a9
Suvonov Javohir <suvonovjavohir625@gmail.com>
2025-05-14 20:13:07 +0500

    update

--- Pages/documents.vue
+++ Pages/documents.vue
@@ -1,5 +1,5 @@
 <template>
     <div>
-        <slot/>
+        documents page
     </div>
 </template>
--- Pages/index.vue
+++ Pages/index.vue
@@ -1,3 +1,7 @@
+<script setup lang="ts">
+
+</script>
 <template>
     <h1>hello nuxt</h1>
+    <NuxtLink to="/about">about</NuxtLink>
 </template>
