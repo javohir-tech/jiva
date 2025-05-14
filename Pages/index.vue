d1f6dac2b17be2c32d54662866b246621b0e539c
Suvonov Javohir <suvonovjavohir625@gmail.com>
2025-05-14 20:46:24 +0500

    update

--- Pages/index.vue
+++ Pages/index.vue
@@ -1,4 +1,6 @@
 <script setup lang="ts">
+import AppButton from '~/Components/AppButton.vue';
+
 useHead({
     title:"Home Page"
 })
@@ -6,5 +8,9 @@ useHead({
 <template>
     <h1>hello nuxt</h1>
     <NuxtLink to="/about">about</NuxtLink> <br>
-    <NuxtLink to="/user">user</NuxtLink>
+    <NuxtLink to="/user">user</NuxtLink><br>
+    <AppButton>
+        salom
+    </AppButton> <br>
+    <SharedButton/>
 </template>
